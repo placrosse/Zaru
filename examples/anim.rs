8452c5f78e5ba941ddf26c0e3957239456aed49a
Sludge <no-email>
2022-04-06 00:53:35 +0200

    animation display demo

---
+++ examples/anim.rs
@@ -0,0 +1,24 @@
+use mizaru::{anim::Animation, gui};
+
+fn main() -> Result<(), mizaru::Error> {
+    let path = std::env::args()
+        .skip(1)
+        .next()
+        .ok_or_else(|| format!("usage: anim <path-to-animation>"))?;
+    let animation = Animation::from_path(&path)?;
+
+    let time_per_frame = animation
+        .frames()
+        .map(|frame| frame.duration().as_secs_f32())
+        .sum::<f32>()
+        / animation.frames().len() as f32;
+    let fps = 1.0 / time_per_frame;
+    eprintln!("~{} FPS", fps);
+
+    for frame in animation.frames().cycle() {
+        gui::show_image("animation", &frame.image_view().to_image());
+        std::thread::sleep(frame.duration());
+    }
+
+    Ok(())
+}
