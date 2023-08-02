44ac0b58eaf0b2e8c808a9afdeb30bd9980a6bad
Sludge <96552222+SludgePhD@users.noreply.github.com>
2023-08-02 03:28:47 +0200

    only save erroneous webcam images when told to

--- crates/zaru/src/video/webcam.rs
+++ crates/zaru/src/video/webcam.rs
@@ -298,7 +298,9 @@ impl Webcam {
                         // ignore for the sake of my own sanity).
                         log::error!("webcam decode error: {}", e);
 
-                        std::fs::write("error.jpg", &*buf).ok();
+                        if let Some(path) = env::var_os("ZARU_WEBCAM_ERROR_DUMP") {
+                            std::fs::write(path, &*buf).ok();
+                        }
 
                         // Hand back a blank image. The alternative would be to skip the image, which
                         // causes 2x latency spikes (OTOH, a blank image isn't going to result in any
